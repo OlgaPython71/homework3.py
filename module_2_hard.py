58ac8e25b6d508e07b79155ac8b4f254b32cdbdd
OlgaPython71 <kuznetsova-o@mail.ru>
2024-08-01 20:11:48 +0300

    Create module_2_hard.py

---
+++ module_2_hard.py
@@ -0,0 +1,17 @@
+import random
+n = random.randint (3,20)
+print(n)
+result = []
+for k in range (1, n):
+    if n % k == 0:
+        for r in range(1, k // 2 + 1):
+            for v in range(1, k):
+                if k == r + v:
+                    result.append(r)
+                    result.append(v)
+for i in range (1, n // 2 + 1):
+    for j in range (1, n):
+        if n == i + j:
+            result.append(i)
+            result.append(j)
+print(result)

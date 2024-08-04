3299ad505f52e50897fa106fdae22ccd2dd3980d
OlgaPython71 <kuznetsova-o@mail.ru>
2024-08-04 18:33:14 +0300

    Create module_3_1_2.py

---
+++ module_3_1_2.py
@@ -0,0 +1,22 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    count_calls()
+    new_string = string.lower()
+    new_list_to_search = [i.lower() for i in list_to_search]
+    return new_string in new_list_to_search
+
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
